c01743bdcb7a0f777ada4b4a33ecf5860722898a
Andy McEniry <andymceniry@outlook.com>
2014-02-03 21:57:18 +0000

    added cordova

--- html/js/app.js
+++ html/js/app.js
@@ -65,7 +65,7 @@ var AM = window.AM || {};
     function onBackKeyDown() {
 		var currentPage = AM.history.pop();
 		alert('xxx'+currentPage+'xxx');
-		if( currentPage != 'home' ) {
+		if( currentPage != "home" ) {
 			var previousPage = AM.history.pop();
 			AM.showPage(previousPage);
 			return false;
