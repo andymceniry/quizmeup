3f0968445f81bc977fdfc52fb76a1f3bd46049f7
Andy McEniry <andymceniry@outlook.com>
2014-02-03 19:41:35 +0000

    added cordova

--- html/js/app.js
+++ html/js/app.js
@@ -1,15 +1,60 @@
-/*globals $*/
+/*global $, window*/
 
-$(function () {
+var AM = window.AM || {};
+
+(function () {
 
 	'use strict';
 
-	$('.button').click(function () {
-		var btn = $(this),
-			ref = btn.data('target-ref');
-		$('.page').hide();
-		$('#page-' + ref).show();
-		return false;
-	});
+	AM.eventHandlerForClosingOverlays = (function () {
+        $(function () {
+			$('body').on('click', '.button', function () {
+				var btn = $(this),
+					pageRef = btn.data('target-ref'),
+					funcRef = btn.data('target-func');
+				if (typeof (pageRef) !== 'undefined') {
+					AM.showPage( pageRef );
+					return false;
+				}
+				if (typeof(funcRef) !== 'undefined') {
+					AM.runFunc( funcRef, btn );
+					return false;
+				}
+			});
+        });
+    }());
 
-});
+
+	AM.showPage = function( pageRef ) {
+		$('.page').hide();
+		$('#page-' + pageRef).show();
+	}
+
+	AM.runFunc = function( funcRef, el ) {
+		AM[funcRef]();
+	}
+	
+	AM.questionCreate = function(el) {
+		alert('yeah baby');
+	}
+
+	
+	function onLoad() {
+        document.addEventListener("deviceready", onDeviceReady, false);
+    }
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        // Register the event listener
+        document.addEventListener("backbutton", onBackKeyDown, false);
+    }
+
+    // Handle the back button
+    //
+    function onBackKeyDown() {
+		alert('back off pal');
+	}
+	
+	
+}());
